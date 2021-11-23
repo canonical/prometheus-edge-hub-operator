9fe64d0ae4fd263710568d1091dd9dd432a37404
Javi Sanchez <javi@Javis-MacBook-Pro.local>
2021-11-23 13:59:15 +0000

    Feat: Adds on_pebble_layer config method

--- src/charm.py
+++ src/charm.py
@@ -6,9 +6,11 @@
 
 import logging
 
+from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
 from ops.charm import CharmBase
 from ops.main import main
 from ops.pebble import Layer
+from ops.model import MaintenanceStatus
 
 logger = logging.getLogger(__name__)
 
@@ -17,11 +19,13 @@ class PrometheusEdgeHubOperatorCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self._service_name = "prometheus-edge-hub"
+        self._container_name= self._service_name = "prometheus-edge-hub"
+        self._container = self.unit.get_container(self._container_name)
         self.framework.observe(
             self.on.prometheus_edge_hub_pebble_ready,
             self._on_prometheus_edge_hub_pebble_ready
         )
+        self._service_patcher = KubernetesServicePatch(self, [("grpc", 9180, 9108)])
 
     def pebble_layer(self):
         return Layer(
@@ -38,7 +42,14 @@ class PrometheusEdgeHubOperatorCharm(CharmBase):
         )
 
     def _on_prometheus_edge_hub_pebble_ready(self, event):
-        pass
+        self.unit.status = MaintenanceStatus("Configuring pod")
+        plan = self._container.get_plan()
+        layer = self._pebble_layer
+        if plan.services != layer.services:
+            self._container.add_layer(self._container_name, layer, combine=True)
+            self._container.restart(self._service_name)
+            logger.info(f"Restarted container {self._service_name}")
+        self.unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
