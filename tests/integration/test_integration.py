b0c014f614843c88929ee8005115e260e734e27c
guillaume <guillaume.belanger27@gmail.com>
2021-11-25 23:02:36 -0500

    Bug fixes in integration tests

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -7,7 +7,7 @@ import time
 from pathlib import Path
 
 import pytest
-import requests
+import requests  # type: ignore
 import yaml
 from pytest_operator.plugin import OpsTest  # type: ignore
 
@@ -20,30 +20,33 @@ PROMETHEUS_APPLICATION_NAME = "prometheus-k8s"
 
 def get_scrape_targets(prometheus_address: str):
     url = f"http://{prometheus_address}:9090/api/v1/targets"
-    response = requests.get(url, timeout=10)
-    response.raise_for_status()
-    return response.json()
-
-
-def validate_scrape_targets(scrape_targets: dict, edge_hub_address: str):
-    active_targets = scrape_targets["data"]["activeTargets"]
-    scrape_urls = [active_target["scrapeUrl"] for active_target in active_targets]
-    edge_hub_metrics_endpoint = f"http://{edge_hub_address}:9091/metrics"
-    assert edge_hub_metrics_endpoint in scrape_urls
-
-
-def validate_scrape_target_is_added_to_prometheus(prometheus_address: str, edge_hub_address: str):
     start_time = time.time()
     timeout = 120
     while time.time() - start_time < timeout:
         try:
-            scrape_targets = get_scrape_targets(prometheus_address)
-            return validate_scrape_targets(scrape_targets, edge_hub_address)
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            return response.json()
         except Exception as e:
             logger.info(e)
             logger.info("Failed retrieving scrape target, will retry in 5 seconds")
         time.sleep(5)
     raise TimeoutError(f"Timeout retrieving scrape target after {timeout} seconds")
+
+
+def validate_scrape_targets(scrape_targets: dict):
+    active_targets = scrape_targets["data"]["activeTargets"]
+    assert len(active_targets) == 2
+    prometheus_edge_hub_target = active_targets[0]
+    assert prometheus_edge_hub_target["scrapePool"].endswith(
+        "prometheus-edge-hub_prometheus_scrape"
+    )
+    assert prometheus_edge_hub_target["scrapeUrl"].endswith(":9091/metrics")
+
+
+def validate_scrape_target_is_added_to_prometheus(prometheus_address: str):
+    scrape_targets = get_scrape_targets(prometheus_address)
+    validate_scrape_targets(scrape_targets)
 
 
 @pytest.mark.abort_on_fail
@@ -68,9 +71,5 @@ async def test_build_and_deploy(ops_test: OpsTest):
         apps=[PROMETHEUS_APPLICATION_NAME], status="active", timeout=1000
     )
     prometheus_k8s_unit = ops_test.model.units[f"{PROMETHEUS_APPLICATION_NAME}/0"]
-    prometheus_edge_hub_unit = ops_test.model.units[f"{APPLICATION_NAME}/0"]
     prometheus_k8s_private_address = prometheus_k8s_unit.data["private-address"]
-    prometheus_edge_hub_private_address = prometheus_edge_hub_unit.data["private-address"]
-    assert validate_scrape_target_is_added_to_prometheus(
-        prometheus_k8s_private_address, prometheus_edge_hub_private_address
-    )
+    validate_scrape_target_is_added_to_prometheus(prometheus_k8s_private_address)
