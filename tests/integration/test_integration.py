59097733448036c08a7b4404ae31f81e1a382b18
Zeeshan Ali Shah <javaclinic@gmail.com>
2022-04-05 19:37:54 +0500

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -87,6 +87,8 @@ async def test_build_and_deploy(ops_test: OpsTest):
 
 
 async def test_remove_relation(ops_test: OpsTest):
-    await ops_test.model.applications["prometheus-edge-hub"].remove_relation("metrics-endpoint","prometheus-k8s")
-    await ops_test.model.wait_for_idle(apps=["prometheus-edge-hub"], status="active", timeout=1000)
+    await ops_test.model.applications["prometheus-edge-hub"].\
+        remove_relation("metrics-endpoint", "prometheus-k8s")
+    await ops_test.model.wait_for_idle\
+        (apps=["prometheus-edge-hub"], status="active", timeout=1000)
 
